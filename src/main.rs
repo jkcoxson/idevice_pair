333912539d1ea4f4c39d3623b76af327423f58ef
CelloSerenity <195480169+CelloSerenity@users.noreply.github.com>
2026-04-14 11:11:46 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -97,7 +97,7 @@ fn supported_apps_for_mode(mode: PairingMode) -> HashMap<String, String> {
             );
             supported_apps.insert("LiveContainer".to_string(), "pairingFile.plist".to_string());
             supported_apps.insert("SparseBox".to_string(), "pairingFile.plist".to_string());
-            supported_apps.insert("ByeTunes".to_string(), "pairingFile.plist".to_string());
+            supported_apps.insert("ByeTunes".to_string(), "pairing file/pairingFile.plist".to_string());
             supported_apps.insert("StikDebug".to_string(), "pairingFile.plist".to_string());
             supported_apps.insert("EnsWilde".to_string(), "pairingFile.plist".to_string());
             supported_apps.insert("StikDebug (Sideloaded)".to_string(), "pairingFile.plist".to_string());
@@ -117,6 +117,7 @@ fn supported_apps_for_mode(mode: PairingMode) -> HashMap<String, String> {
             supported_apps.insert("Protokolle".to_string(), "pairingFile.plist".to_string());
             supported_apps.insert("Antrag".to_string(), "pairingFile.plist".to_string());
             supported_apps.insert("Feather".to_string(), "pairingFile.plist".to_string());
+            supported_apps.insert("Reynard".to_string(), "pairingFile.plist".to_string());
         }
     }
     supported_apps
